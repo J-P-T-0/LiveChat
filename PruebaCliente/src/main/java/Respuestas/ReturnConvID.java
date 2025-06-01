24ff4af2bf70c34423b5298f7591c1a7c78bbc2a
Daniel <183682@upslp.edu.mx>
2025-06-01 17:22:11 -0600

    rework a la respuesta de crear conversacion

--- PruebaCliente/src/main/java/Respuestas/ReturnConvID.java
+++ PruebaCliente/src/main/java/Respuestas/ReturnConvID.java
@@ -4,13 +4,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ReturnConvID extends Respuesta {
     private int convID;
+    private String destinatario;
 
-    public ReturnConvID(@JsonProperty("convID") int convID) {
+    public ReturnConvID(@JsonProperty("convID") int convID,
+                        @JsonProperty("destinatario") String destinatario) {
         super("RETURN_CONV_ID");
         this.convID = convID;
+        this.destinatario = destinatario;
     }
 
     public int getConvID() {
         return convID;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
     }
 }
